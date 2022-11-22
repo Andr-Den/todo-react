80b8ac5a6104764f92ede720986a81ef1fd23512
Andrey Denisov <86472463+Andr-Den@users.noreply.github.com>
2022-11-22 19:21:59 +0500

    connect mongoDB

--- backend/app.js
+++ backend/app.js
@@ -1,8 +1,17 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
 
 const { PORT = 3001 } = process.env;
 
 const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+mongoose.connect('mongodb://localhost:27017/todoDB');
+
+app.use('/', require('./routes/points'));
 
 app.listen(PORT, () => {
   // eslint-disable-next-line no-console
