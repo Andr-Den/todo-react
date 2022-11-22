3f91225d5d206857d74b8ab58185f7151e23b78f
Andrey Denisov <86472463+Andr-Den@users.noreply.github.com>
2022-11-22 17:12:01 +0500

    add components to App

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import Header from './components/Header';
+import TodoList from './components/TodoList'
+import Popup from './components/Popup';
 
 function App() {
+
+  const [items, setItems] = React.useState([{ title: 'Почистить зубы', description: '', date: '11.12.13' }])
+  const [popupOpen, setPopupOpen] = React.useState(false)
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <TodoList items={items} setItems={items}/>
+      {popupOpen ? <Popup /> : ''}
     </div>
   );
 }
